4e7c967f84b3f9b9d27e0a23129411bdd6ecf933
jishithachowdary <nemanijc@gmail.com>
2021-06-04 11:01:12 +0530

    first

---
+++ greetotdoor/src/main/java/com/greetotdoor/GreetotdoorApplication.java
@@ -0,0 +1,13 @@
+package com.greetotdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetotdoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetotdoorApplication.class, args);
+	}
+
+}
