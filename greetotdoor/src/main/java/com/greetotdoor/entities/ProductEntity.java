076697bf49cfc972cff8f178b19d0c11236be3fe
modinilalitha <modinilalitha@gmail.com>
2021-06-04 11:29:51 +0530

    updated product

---
+++ greetotdoor/src/main/java/com/greetotdoor/entities/ProductEntity.java
@@ -0,0 +1,102 @@
+package com.greetotdoor.entities;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ProductEntity")
+public class ProductEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="productId")
+	private int productId;
+	@Column(name="productName")
+	private String productName;
+	@Column(name="price")
+	double price;
+	@Column(name="image")
+	String image;
+	@Column(name="colour")
+	String colour;
+	@Column(name="category")
+	String category;
+	@Column(name="quantity")
+	int quantity;
+	@Column(name="manufacturer")
+	String manufacturer;
+	@Column(name="specification")
+	String specification;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getSpecification() {
+		return specification;
+	}
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+	@Override
+	public String toString() {
+		return "ProductEntity [productId=" + productId + ", productName=" + productName + ", price=" + price
+				+ ", image=" + image + ", colour=" + colour + ", category=" + category + ", quantity=" + quantity
+				+ ", manufacturer=" + manufacturer + ", specification=" + specification + "]";
+	}
+	
+	
+	
+
+}
+
+
+
